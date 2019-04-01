d447becd3c0d84460e1d9d7d082e61f07acb20a5
muracafernando <muracafernando@gmail.com>
2019-04-01 12:40:33 -0300

    Add files via upload

---
+++ src/main/java/ar/utn/edu/tacs/TacsApplication.java
@@ -0,0 +1,13 @@
+package ar.utn.edu.tacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacsApplication.class, args);
+	}
+
+}
