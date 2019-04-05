f065b375c815d5afb0e2d072b9c39fcad9ccd4c2
Kevin Furman <kfurman@despegar.com>
2019-04-04 22:53:39 -0300

    Creo controllers y agrego comentarios con las user stories relacionadas para ir creando los servicios rest que las implementen

---
+++ src/main/java/ar/utn/edu/tacs/ListsController.java
@@ -0,0 +1,14 @@
+package ar.utn.edu.tacs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ListsController {
+
+//    Como usuario quiero poder registrar un lugar que me interesa en una de mis listas de lugares.
+//    Como usuario quiero crear, eliminar o cambiar el nombre a las diferentes listas de lugares que poseo.
+//    Como usuario quiero marcar en una lista los lugares a los que ya fui (sin eliminarlos de la lista)
+
+}
--- src/main/java/ar/utn/edu/tacs/UserController.java
+++ src/main/java/ar/utn/edu/tacs/UserController.java
@@ -1,6 +1,7 @@
 package ar.utn.edu.tacs;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +13,9 @@ public class UserController {
     public String exampleGet() {
         return "get Example ";
     }
+
+    // Como usuario quiero poder crear una cuenta con user y pass. Nota: La cuenta es de nuestra aplicación, no de Foursquare. La aplicación manejará una única cuenta de Foursquare para acceder a los recursos necesarios.
+    // Como usuario quiero hacer log in en la página
+    // Como usuario quiero hacer log out en la página
+
 }
