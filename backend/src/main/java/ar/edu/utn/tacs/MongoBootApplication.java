4247dc9c55a1fa921e822b746548438dca4dc0c0
matias <example@example>
2019-05-30 17:08:47 -0300

    Remove conflicting annotation

--- backend/src/main/java/ar/edu/utn/tacs/MongoBootApplication.java
+++ backend/src/main/java/ar/edu/utn/tacs/MongoBootApplication.java
@@ -18,7 +18,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 *
 * */
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MongoBootApplication implements CommandLineRunner {
 
     @Autowired
