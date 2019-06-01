6338444cb62a05de3faee5501b47a7a34a77ec28
Fernando Muraca <muracafernando@gmail.com>
2019-06-01 15:52:32 -0300

    Persistencia de usuarios nuevos
    para crear el user -> http://localhost:8080/#/signup
    
    para logearse -> http://localhost:8080/login

--- backend/src/main/java/ar/edu/utn/tacs/controller/UserController.java
+++ backend/src/main/java/ar/edu/utn/tacs/controller/UserController.java
@@ -1,8 +1,13 @@
 package ar.edu.utn.tacs.controller;
 
+import ar.edu.utn.tacs.config.SecurityConfiguration;
 import ar.edu.utn.tacs.model.UserSession;
 import ar.edu.utn.tacs.model.Users;
+import ar.edu.utn.tacs.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,9 +15,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/user")
 public class UserController {
 
+    @Autowired
+    UserRepository repository;
+
+    @Autowired
+    SecurityConfiguration configuration;
+
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public Users create(@RequestBody Users user) {
+        user.setPassword(configuration.passwordEncoder().encode(user.getPassword()));
+        user.setAdmin(false);//TODO establecer por signup si es o no admin
+        repository.save(user);
         return user;
     }
 
